f54d8b762e4a8e29cc04d6257cfe7aef5537c2b7
SMITHBOUZI <36991006+SMITHBOUZI@users.noreply.github.com>
2018-05-09 05:35:14 +0200

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,10 @@
+
+ 
+$(document).ready(function(){
 $(document).foundation();
 
 $("#login").click(function(e){
+	$("#nav_menu").hide(10);
 
 $("#search").hide(100);	
 
@@ -9,14 +13,31 @@ $("#user").slideToggle(350);
 });
 
 
-
-
 $("#search_button").click(function(e){
 $("#user").hide(100);
-	
+$("#nav_menu").hide(10);
 
-$("#search").slideToggle(350);
+$("#search").slideToggle(500);
 
 });
+
+
+
+// Resnponsive
+
+
+$(".menu-icon").click(function(e){
+	$("#search").hide(10);
+	$("#user").hide(10);
+
+
+$("#nav_menu").slideToggle(400);
+
+});
+
+
+
+});
+
 
 
