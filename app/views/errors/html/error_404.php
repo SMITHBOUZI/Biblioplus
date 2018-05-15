60977f3284594783098b6336b4b852327f0d1038
Bergele Franck <franckoberg@gmail.com>
2018-05-15 14:56:43 -0400

    personnalisation de la d'erreur error_404.php

--- app/views/errors/html/error_404.php
+++ app/views/errors/html/error_404.php
@@ -1,64 +1,80 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
+
+?>
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
 <meta charset="utf-8">
-<title>404 Page Not Found</title>
-<style type="text/css">
+<title>404 Page Not Found</title> -->
 
-::selection { background-color: #E13300; color: white; }
-::-moz-selection { background-color: #E13300; color: white; }
+<!doctype html>
 
-body {
-	background-color: #fff;
-	margin: 40px;
-	font: 13px/20px normal Helvetica, Arial, sans-serif;
-	color: #4F5155;
-}
+<html class="no-js" lang="en">
+  <head>
+      <meta charset="utf-8">
+            <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>BiBlioPlus</title>
+        <link rel="stylesheet" href="assets/css/app.css">
+        <script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
+        <link href="https://fonts.googleapis.com/css?family=Barlow+Condensed|Bevan|Carter+One|Fira+Sans+Extra+Condensed|Fjalla+One|Fredoka+One|Hind+Madurai|Oswald|Palanquin+Dark|Paytone+One|Ramabhadra|Suez+One" rel="stylesheet">
+  </head>
 
-a {
-	color: #003399;
-	background-color: transparent;
-	font-weight: normal;
-}
-
-h1 {
-	color: #444;
-	background-color: transparent;
-	border-bottom: 1px solid #D0D0D0;
-	font-size: 19px;
-	font-weight: normal;
-	margin: 0 0 14px 0;
-	padding: 14px 15px 10px 15px;
-}
-
-code {
-	font-family: Consolas, Monaco, Courier New, Courier, monospace;
-	font-size: 12px;
-	background-color: #f9f9f9;
-	border: 1px solid #D0D0D0;
-	color: #002166;
-	display: block;
-	margin: 14px 0 14px 0;
-	padding: 12px 10px 12px 10px;
-}
-
-#container {
-	margin: 10px;
-	border: 1px solid #D0D0D0;
-	box-shadow: 0 0 8px #D0D0D0;
-}
-
-p {
-	margin: 12px 15px 12px 15px;
-}
-</style>
-</head>
 <body>
-	<div id="container">
-		<h1><?php echo $heading; ?></h1>
-		<?php echo $message; ?>
+		<header>
+		    <div class="grid-container" > 
+		    	<div class="grid-x ">
+		       		<button class="menu-icon show-for-small-only"></button>   
+					<div class="cell medium-12 large-2  small-2" style="text-align:center;">
+						<div id="logo" >
+							<img src="assets/img/BPlogo.png" style="max-width:160px;">
+						</div>
+		            </div>                
+		            <nav class="cell small-10 medium-9 large-8 ">
+		              <ul class="  menu align-center"  id="nav_menu">
+		                    <li><a href="#"><i class="fas fa-home " ></i></a></li>
+		                    <li><a href="#">Collection</a></li>
+		                    <li><a href="#">Auteurs</a></li>
+		                    <li><a href="#">Evenements</a></li>
+		                    <li><a href="forum/index">Forum</a></li>
+		              </ul>                  
+		            </nav>
+		            <div class="cell small-4  medium-2 large-2">
+		                <section class="menu align-center" style="padding:0px;" id="search_login">
+			                <!-- <li> <a  id="login"><i class="fas fa-user-circle  "></i></a></li> -->
+			                <li><a id="search_button" ><i class="fas fa-search" ></i></a></li>
+							    <div id="search_box" ><!-- Drop down search -->
+							    	<div id="search" > 
+										<form >
+										  <div class="grid-container">
+										    <div class="grid-x padding-x">
+										    	<div class="medium-12 cell">
+										       		 <input class="custom_input" type="text" placeholder="Rechercher" >						        
+										      	</div>
+										  	</div>
+										  </div>
+										</form>
+									</div>   
+								</div> <!-- End of dropdwown_search -->
+					    </section> <!-- end of section menu -->      
+		            </div>
+		        </div><!--end of grid-->
+		    </div><!--end of grid-container-header-->
+		</header>
+</div>
+
+<script src="assets/node_modules/jquery/dist/jquery.js "></script>
+<script src="assets/node_modules/what-input/dist/what-input.js "></script>
+<script src="assets/node_modules/foundation-sites/dist/js/foundation.js "></script>
+<script src="assets/js/app.js "></script>
+
+</style> 
+    <div class="row">
+
+	<div class="colunms large-12 medium-12 small-12">
+		<img  src="assets/img/404-error-page-not-found.jpg">
+	</div>
 	</div>
 </body>
-</html>
+</html> 
